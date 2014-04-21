8b66bb3d44f9f18bb8c1374aa2ccbef311adddbe
Artemiy Garin <kvirair@gmail.com>
2014-04-21 21:01:03 +0400

    - Initial commit

---
+++ src/garin/artemiy/quickaction/example/MainActivity.java
@@ -0,0 +1,15 @@
+package garin.artemiy.quickaction.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+import garin.artemiy.quickaction.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_layout);
+    }
+
+}
