678e82851ddba3b14adcccb7b41961f82f3d2942
Artemiy Garin <developer.garin@gmail.com>
2015-04-01 15:59:30 +0300

    — Refactor

--- src/garin/artemiy/quickaction/example/MainActivity.java
+++ src/garin/artemiy/quickaction/example/MainActivity.java
@@ -17,8 +17,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_layout);
 
-        RelativeLayout customLayout = (RelativeLayout) getLayoutInflater().inflate(R.layout.popup_custom_layout, null);
-        quickAction = new QuickAction(this, R.style.PopupAnimation, customLayout, customLayout);
+        RelativeLayout customLayout = (RelativeLayout)
+                getLayoutInflater().inflate(R.layout.popup_custom_layout, null);
+
+        quickAction = new QuickAction
+                (this, R.style.PopupAnimation, customLayout, customLayout);
     }
 
     @SuppressWarnings("unused")
